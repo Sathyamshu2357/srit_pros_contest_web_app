f83749b10ee322027c71aefea261fa81a3a8bf56
unknown <sathyamshu2357@gmail.com>
2021-05-22 19:47:35 +0530

    basic auth

---
+++ details.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCa6zBtipVfaQDQQsspFN723yMWgSKWSPg",
+    authDomain: "srit-pros-contest.firebaseapp.com",
+    projectId: "srit-pros-contest",
+    storageBucket: "srit-pros-contest.appspot.com",
+    messagingSenderId: "1025128489593",
+    appId: "1:1025128489593:web:a386549ba8f46955afaaa8",
+    measurementId: "G-42N2X8XVT1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
